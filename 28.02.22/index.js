6eda902e670afd94754bb95eee59a14e7e6eaf57
Нафис Гарифуллин <you@example.com>
2022-03-02 00:47:32 +0500

    упражнение 1-5 сделал

---
+++ 28.02.22/index.js
@@ -0,0 +1,26 @@
+//Упражнение 1
+// let number = 10+5;
+// alert(number);
+
+//Упражнение 2
+// const name = prompt ("Ваше ФИО");
+
+// //Упражнение 3
+// console.log(typeof 3);
+// console.log(typeof "Всем привет");
+
+//Упражнение 4
+// let a = +prompt("Введите число a");
+// let b = +prompt("Введите число b");
+
+// const c = (a + b);
+// alert(c);
+
+//Упражнение 5
+// let a = +prompt("Введите число a");
+// let b = +prompt("Введите число b");
+
+// const c = (a / b);
+// alert(c);
+
+
